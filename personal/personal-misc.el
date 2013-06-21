15aad4081b6e3dfbe0254aa9b097a721dda2ab80
Sean Fisk <sean@seanfisk.com>
2013-06-21 16:20:33 -0600

    Make the compilation buffer follow to first error.

---
+++ personal/personal-misc.el
@@ -0,0 +1,34 @@
+;;; personal-misc.el --- Miscellaneous customizations
+;;
+;; Author: Sean Fisk
+;; Maintainer: Sean Fisk
+;; Keywords: convenience
+;; Compatibility: GNU Emacs: 24.x, Aquamacs: 3.x
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(setq compilation-scroll-output 'first-error)
+
+(provide 'personal-misc)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; personal-misc.el ends here

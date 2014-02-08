c3d57be1dc14bd4eba0c1bfe315d6713d0b77fff
Sean Fisk <sean@seanfisk.com>
2014-02-08 00:52:29 -0500

    Rebind mc/edit-lines. Subject to change.
    The current binding was not working under OS X. I think the operating
    system was capturing it.

--- personal/personal-packages.el
+++ personal/personal-packages.el
@@ -218,7 +218,7 @@
 ;;; multiple-cursors
 (use-package multiple-cursors
   :ensure t
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
+  :bind (("C-c m" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this-dwim)))

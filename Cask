2cbd757f87a31cce2757950ab3221ad34ab3e6c9
Sean Fisk <sean@seanfisk.com>
2015-08-14 16:04:18 -0400

    Clean up Cask sources/packages; disable powerline.

--- Cask
+++ Cask
@@ -1,12 +1,30 @@
-(source melpa-stable)
+;;; Package archives
+;;
+;; When sourcing MELPA, its versions will take precedence over all of
+;; MELPA Stable's. Unfortunately, there are some packages that are not
+;; available on MELPA Stable, and also not available through fetchers
+;; (e.g., packages on the wiki). There is currently no way through
+;; Cask to pin a package to a specific source. For now, we're just
+;; installing everything from MELPA, even though we'd like to install
+;; with preference from MELPA Stable.
+;;
+;; Hopefully source-based selection will be available soon:
+;; https://github.com/cask/cask/issues/212
+;;
+;; Order appears not to matter for this list. Packages are chosen by
+;; the highest version.
+(source gnu) ; for let-alist
+(source melpa)
 (source org)
 
-;; Prelude packages
+
+
+;;; Prelude packages
 ;;
 ;; These packages are required because they are used for Prelude. This
 ;; list is copied from 'core/prelude-packages.el'.
-(depends-on "ace-window")
-(depends-on "avy")
+(depends-on "ace-window") ; chosen over `switch-window' and `buffer-move'
+(depends-on "avy") ; chosen over `ace-jump-mode'
 (depends-on "anzu")
 (depends-on "browse-kill-ring")
 (depends-on "dash")
@@ -31,7 +49,7 @@
 (depends-on "operate-on-number")
 (depends-on "smartparens")
 (depends-on "smartrep")
-(depends-on "smart-mode-line")
+(depends-on "smart-mode-line") ; chosen over `powerline'
 (depends-on "undo-tree")
 (depends-on "volatile-highlights")
 (depends-on "zenburn-theme")
@@ -44,8 +62,9 @@
 (depends-on "haskell-mode") ; haskell
 (depends-on "helm") ; helm
 (depends-on "helm-projectile") ; helm
-; helm-descbinds on MELPA-stable is old
-(depends-on "helm-descbinds" :git "https://github.com/emacs-helm/helm-descbinds.git") ; helm-everywhere
+;; helm-descbinds on MELPA-stable is old, pull directly from Git.
+;;(depends-on "helm-descbinds" :git "https://github.com/emacs-helm/helm-descbinds.git") ; helm-everywhere
+(depends-on "helm-descbinds")
 (depends-on "helm-ag") ; helm-everywhere
 (depends-on "js2-mode") ; js
 (depends-on "json-mode") ;js
@@ -53,19 +72,22 @@
 (depends-on "rainbow-delimiters") ; lisp
 (depends-on "ruby-tools") ; ruby
 (depends-on "inf-ruby") ; ruby
-(depends-on "yari") ; ruby
+;;(depends-on "yari" :git "https://github.com/hron/yari.el.git") ; ruby
+(depends-on "yari")
 (depends-on "geiser") ; scheme
 (depends-on "scss-mode") ; scss
 (depends-on "web-mode") ; web
 (depends-on "yaml-mode") ; yaml
 
-;; My required packages
+
+
+;;; My required packages
 (depends-on "auctex")
-(depends-on "buffer-move")
-(depends-on "cask")
+(depends-on "bind-key")
 (depends-on "cmake-mode")
 (depends-on "csv-mode")
-(depends-on "dtrt-indent" :git "https://github.com/jscheid/dtrt-indent.git")
+(depends-on "dash-at-point")
+(depends-on "dtrt-indent")
 (depends-on "editorconfig")
 (depends-on "ein")
 (depends-on "f")
@@ -81,9 +103,9 @@
 (depends-on "jump-char")
 (depends-on "markdown-mode")
 (depends-on "multiple-cursors")
-(depends-on "nyan-mode" :git "https://github.com/TeMPOraL/nyan-mode.git")
+;; nyan-mode can't install because it's missing a package version.
+;; (depends-on nyan-mode)
 (depends-on "paredit")
-(depends-on "powerline")
 (depends-on "s")
 ;; The difference between `smooth-scroll' and `smooth-scrolling' is
 ;; this: `smooth-scroll' changes "Page Up" and "Page Down" to show all
@@ -93,11 +115,8 @@
 ;; want, because it allows viewing the context of the line with the
 ;; cursor.
 (depends-on "smooth-scrolling")
-;; This package has no autoloads, so it doesn't "do" anything when
-;; it's just installed through ELPA. It needs to be required, at which
-;; point it takes over the `C-x o' binding.
-(depends-on "switch-window")
 (depends-on "undo-tree")
 (depends-on "unfill")
+(depends-on "use-package")
 (depends-on "whole-line-or-region")
 (depends-on "yasnippet")
--- personal/personal-packages.el
+++ personal/personal-packages.el
@@ -130,80 +130,80 @@
 (use-package paredit
   :diminish paredit-mode)
 
-(use-package powerline
-  :config (progn
-          (defun powerline-sean-theme ()
-            "Set up mode line with nyan-mode."
-            ;; This is mostly a copy of `powerline-default-theme' with
-            ;; nyan-mode added.
-            (interactive)
-            (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-                          (face2 (if active 'powerline-active2 'powerline-inactive2))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          powerline-default-separator
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           powerline-default-separator
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list
-                                ;; output read-only (%), modified (*), unmodified (-)
-                                (powerline-raw "%*" nil 'l)
-                                (powerline-buffer-size nil 'l)
-                                (powerline-raw mode-line-mule-info nil 'l)
-                                (powerline-buffer-id nil 'l)
-                                (when (and (boundp 'which-func-mode) which-func-mode)
-                                  (powerline-raw which-func-format nil 'l))
-                                (powerline-raw " ")
+;; (use-package powerline
+;;   :config (progn
+;;           (defun powerline-sean-theme ()
+;;             "Set up mode line with nyan-mode."
+;;             ;; This is mostly a copy of `powerline-default-theme' with
+;;             ;; nyan-mode added.
+;;             (interactive)
+;;             (setq-default mode-line-format
+;;                 '("%e"
+;;                   (:eval
+;;                    (let* ((active (powerline-selected-window-active))
+;;                           (mode-line (if active 'mode-line 'mode-line-inactive))
+;;                           (face1 (if active 'powerline-active1 'powerline-inactive1))
+;;                           (face2 (if active 'powerline-active2 'powerline-inactive2))
+;;                           (separator-left (intern (format "powerline-%s-%s"
+;;                                                           powerline-default-separator
+;;                                                           (car powerline-default-separator-dir))))
+;;                           (separator-right (intern (format "powerline-%s-%s"
+;;                                                            powerline-default-separator
+;;                                                            (cdr powerline-default-separator-dir))))
+;;                           (lhs (list
+;;                                 ;; output read-only (%), modified (*), unmodified (-)
+;;                                 (powerline-raw "%*" nil 'l)
+;;                                 (powerline-buffer-size nil 'l)
+;;                                 (powerline-raw mode-line-mule-info nil 'l)
+;;                                 (powerline-buffer-id nil 'l)
+;;                                 (when (and (boundp 'which-func-mode) which-func-mode)
+;;                                   (powerline-raw which-func-format nil 'l))
+;;                                 (powerline-raw " ")
 
-                                (funcall separator-left mode-line face1)
+;;                                 (funcall separator-left mode-line face1)
 
-                                (when (boundp 'erc-modified-channels-object)
-                                  (powerline-raw erc-modified-channels-object face1 'l))
-                                (powerline-major-mode face1 'l)
-                                (powerline-process face1)
-                                (powerline-minor-modes face1 'l)
-                                (powerline-narrow face1 'l)
-                                (powerline-raw " " face1)
+;;                                 (when (boundp 'erc-modified-channels-object)
+;;                                   (powerline-raw erc-modified-channels-object face1 'l))
+;;                                 (powerline-major-mode face1 'l)
+;;                                 (powerline-process face1)
+;;                                 (powerline-minor-modes face1 'l)
+;;                                 (powerline-narrow face1 'l)
+;;                                 (powerline-raw " " face1)
 
-                                (funcall separator-left face1 face2)
+;;                                 (funcall separator-left face1 face2)
 
-                                (powerline-vc face2 'r)))
-                          (rhs (list
-                                (powerline-raw global-mode-string face2 'r)
+;;                                 (powerline-vc face2 'r)))
+;;                           (rhs (list
+;;                                 (powerline-raw global-mode-string face2 'r)
 
-                                (funcall separator-right face2 face1)
+;;                                 (funcall separator-right face2 face1)
 
-                                ;; line number
-                                (powerline-raw "%4l" face1 'l)
-                                (powerline-raw ":" face1 'l)
-                                ;; column number
-                                (powerline-raw "%3c" face1 'r)
+;;                                 ;; line number
+;;                                 (powerline-raw "%4l" face1 'l)
+;;                                 (powerline-raw ":" face1 'l)
+;;                                 ;; column number
+;;                                 (powerline-raw "%3c" face1 'r)
 
-                                (funcall separator-right face1 mode-line)
+;;                                 (funcall separator-right face1 mode-line)
 
-                                ;; Look here for hints:
-                                ;; <http://nyan-mode.buildsomethingamazing.com/#sec-4>
-                                ;; <https://github.com/bodil/emacs.d/blob/master/bodil-theme.el#L188>
-                                (powerline-raw " ")
-                                (powerline-raw (nyan-create))
-                                ;; (powerline-raw "%6p" nil 'r)
-                                ;; (powerline-hud face2 face1)
-                                )))
-                     (concat
-                      (powerline-render lhs)
+;;                                 ;; Look here for hints:
+;;                                 ;; <http://nyan-mode.buildsomethingamazing.com/#sec-4>
+;;                                 ;; <https://github.com/bodil/emacs.d/blob/master/bodil-theme.el#L188>
+;;                                 (powerline-raw " ")
+;;                                 (powerline-raw (nyan-create))
+;;                                 ;; (powerline-raw "%6p" nil 'r)
+;;                                 ;; (powerline-hud face2 face1)
+;;                                 )))
+;;                      (concat
+;;                       (powerline-render lhs)
 
-                      (powerline-fill face2 (powerline-width rhs))
+;;                       (powerline-fill face2 (powerline-width rhs))
 
-                      (powerline-render rhs)))))))
-          ;; Run this after everything initializes or else it will
-          ;; look wonky.
-          (add-hook 'after-init-hook 'powerline-sean-theme t)
-	  ))
+;;                       (powerline-render rhs)))))))
+;;           ;; Run this after everything initializes or else it will
+;;           ;; look wonky.
+;;           (add-hook 'after-init-hook 'powerline-sean-theme t)
+;; 	  ))
 
 (use-package undo-tree
   :diminish undo-tree-mode
